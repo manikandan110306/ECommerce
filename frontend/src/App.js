eabcb1731178954f0addb40abc333915e197cce6
Manikandan T <manit110306@gmail.com>
2026-01-31 23:00:09 +0530

    product details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,23 @@ import Footer from './components/layouts/Footer';
 import Header from './components/layouts/Header';
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ProductDetail from './components/product/ProductDetail';
+
 function App() {
   return (
     <HelmetProvider>
       <Router>
         <div className="App">
           <Header/>
-            <Routes>
-              <Route exact path='/' element={<Home/>}/>
-            </Routes>
+            <div className='container container-fluid'>
+              <ToastContainer theme='dark'/>
+              <Routes>
+                <Route exact path='/' element={<Home/>}/>
+                <Route exact path='/product/:id' element={<ProductDetail/>}/>
+              </Routes>
+            </div>
           <Footer/>
         </div>
       </Router>
