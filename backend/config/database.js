09652922fd45807ef30f27ddc4ecfd63ce7a2a4b
Manikandan T <manit110306@gmail.com>
2026-02-21 23:14:14 +0530

    databse error fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
-const connectDatabase = () => {
-    mongoose.set("strictQuery", false);
+const connectDatabase = async () => {
+    try {
+        mongoose.set("strictQuery", false);
 
-    mongoose.connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(con => {
+        const con = await mongoose.connect(process.env.DB_URI);
+
         console.log(`✅ MongoDB connected: ${con.connection.host}`);
-    }).catch(err => {
-        console.error("❌ MongoDB Connection Error:", err);
+
+    } catch (err) {
+        console.error("❌ MongoDB connection FAILED:", err.message);
         process.exit(1);
-    });
+    }
 };
 
 module.exports = connectDatabase;
